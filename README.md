5ff955a5dc6f823e9688a61069aacdfa2ebf7802
Nikilauz <myinternetid@gmx.de>
2021-04-03 19:46:22 +0200

    finally get to work permissions during install

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # deadman.sh
 A tiny zenity-based bash daemon checking if you stay alive while sitting in front of your screen...
 ## Installation
-`wget -qO - https://raw.githubusercontent.com/Nikilauz/deadman.sh/main/deadman_install.sh | sudo bash`
+`wget -qO - https://raw.githubusercontent.com/Nikilauz/deadman.sh/main/deadman_install.sh | bash`
 ## Configuration (optional)
  - Put desired timer duration in minutes as simple number into `~/.config/deadman/deadline.txt`.
  - Put desired countdown duration in seconds as number into `~/.config/deadman/final_countdown.txt`.
--- deadman_install.sh
+++ deadman_install.sh
@@ -7,13 +7,13 @@ ALARM=alarm.ogg
 BIN=/usr/local/bin
 SHARE=/usr/local/share/deadman
 
-rm -f $BIN/$MAIN $BIN/$DAEMON
-wget -P $BIN $SOURCE/$MAIN $SOURCE/$DAEMON
-chmod 755 $BIN/$MAIN $BIN/$DAEMON
+sudo rm -f $BIN/$MAIN $BIN/$DAEMON
+sudo wget -P $BIN $SOURCE/$MAIN $SOURCE/$DAEMON
+sudo chmod 755 $BIN/$MAIN $BIN/$DAEMON
 
-rm -r -f $SHARE
-mkdir -p $SHARE
-wget -P $SHARE $SOURCE/$ALARM
+sudo rm -r -f $SHARE
+sudo mkdir -p $SHARE
+sudo wget -P $SHARE $SOURCE/$ALARM
 
 zenity --question --cancel-label="Ok" --ok-label="Run deadman" --title="deadman" --width=250 --icon-name=face-devilish \
     --text="Everything is done - thanks for installing!"
